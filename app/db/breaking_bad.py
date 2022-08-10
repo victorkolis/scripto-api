ce1b6d2d8250d9ed93210fc8c7df0f06e9a693c9
victorkolis <victorkolis@gmail.com>
2022-08-10 01:48:17 -0300

    initial commit

---
+++ app/db/breaking_bad.py
@@ -0,0 +1,56 @@
+# TODO: Replace this file for a json one
+# TODO: 10 Characters per page
+
+data = {
+    'info': {
+        'count': 0,
+        'pages': 0,
+        'next': '',
+        'previous': '',
+
+    },
+    'results': [
+        {
+            'id': 1,
+            'name': 'Walter Hartwell White',
+            'status': 'unknown',
+            'gender': 'Male',
+            'actor': 'Bryan Cranston'
+        },
+        {
+            'id': 2,
+            'name': 'Jesse Bruce Pinkman',
+            'status': 'alive',
+            'gender': 'Male',
+            'actor': 'Aaron Paul'
+        },
+        {
+            'id': 3,
+            'name': 'Skyler White',
+            'status': 'alive',
+            'gender': 'Female',
+            'actor': 'Anna Gunn'
+        },
+        {
+            'id': 4,
+            'name': 'Henry "Hank" R. Schrader',
+            'status': 'dead',
+            'gender': 'Male',
+            'actor': 'Dean Norris'
+        },
+        {
+            'id': 5,
+            'name': 'Marie Schrader',
+            'status': 'alive',
+            'gender': 'Female',
+            'actor': 'Betsy Brandt'
+        },
+        {
+            'id': 6,
+            'name': 'Walter White Jr.',
+            'status': 'alive',
+            'gender': 'Male',
+            'actor': 'RJ Mitte'
+        }
+    ]
+}
